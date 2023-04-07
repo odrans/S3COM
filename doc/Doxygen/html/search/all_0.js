d865fdf667166fbfea957e28eca8b8235213f71a
Odran Sourdeval <odran.sourdeval@univ-lille.fr>
2023-04-07 10:44:50 +0200

    switch to no sidebar-only

--- doc/Doxygen/html/search/all_0.js
+++ doc/Doxygen/html/search/all_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['cld_5fmie_5fload_0',['cld_mie_load',['../namespacemod__cld__mie.html#ab52fe6d2fa8b4c0b89e1faad61fb1eb5',1,'mod_cld_mie']]],
-  ['cld_5fmie_5fread_1',['cld_mie_read',['../namespacemod__cld__mie.html#a527f0f6d5d6f03458e2905a1c6061701',1,'mod_cld_mie']]]
+  ['add_5faerosols_0',['add_aerosols',['../structs3com__types_1_1type__nml.html#a4b2fa8f0bf0d9768c7a4f9509ef40fae',1,'s3com_types::type_nml']]],
+  ['add_5fclouds_1',['add_clouds',['../structs3com__types_1_1type__nml.html#acefcf5adb9c3aaa3eca9f7870e7b52fa',1,'s3com_types::type_nml']]],
+  ['add_5frefrac_2',['add_refrac',['../structs3com__types_1_1type__nml.html#acdf1c6762e48595fb232e7baf7b055c7',1,'s3com_types::type_nml']]]
 ];
--- doc/Doxygen/html/search/searchdata.js
+++ doc/Doxygen/html/search/searchdata.js
@@ -1,21 +1,30 @@
 var indexSectionsWithContent =
 {
-  0: "cms",
-  1: "m",
-  2: "cs"
+  0: "acdfgimnoprstuv",
+  1: "tv",
+  2: "m",
+  3: "cs",
+  4: "acdfgimnoprsuv",
+  5: "s"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "namespaces",
-  2: "functions"
+  1: "classes",
+  2: "namespaces",
+  3: "functions",
+  4: "variables",
+  5: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Namespaces",
-  2: "Functions"
+  1: "Classes",
+  2: "Namespaces",
+  3: "Functions",
+  4: "Variables",
+  5: "Pages"
 };
 
